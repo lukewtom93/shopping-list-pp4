17b7a968b10257de6f6ceaf8a61fd89e47f3788b
lukewtom93 <lukewtom93@gmail.com>
2025-05-14 01:32:52 +0100

    added test for login and registratition authentication

--- shopping_list/tests.py
+++ shopping_list/tests.py
@@ -1,47 +1,22 @@
 from django.test import TestCase
-from django.urls import reverse
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 
 
-# Automated test for register view
-class RegisterViewTests(TestCase):
-    def setUp(self):
-        """
-        Setup tests
-        """
-        self.url = reverse('register')
-
-    def unauthenticatedAccessTest(self):
-        """
-        Sends GET request and confirms the right
-        template and access pages are used
-        """
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'shopping_list/register.html')
-        self.assertIsInstance(response.context['form'], UserCreationForm)
-
-    def authenticatedRedirectTest(self):
-        """
-        Creates a user to test the correct redirects are in place
-        """
-        test_user = User.objects.create_user(username='testuser',
-                                             password='password')
-        self.client.login(username='testuser', password='password')
-        response = self.client.get(self.url)
-        self.assertRedirects(response, reverse('shopping-list'))
-
-    def RegistrationTest(self):
-        """
-        Sends a POST request to confirm the registration
-        page is creating a new user
-        """
-        response = self.client.post(self.url, {
-            'username': 'newuser',
-            'password1': 'Password123',
-            'password2': 'Password123',
+class AuthenticationTests(TestCase):
+    def test_user_registration(self):
+        response = self.client.post('/register/', {
+            'username': 'testuser',
+            'password1': 'StrongPass123',
+            'password2': 'StrongPass123'
         })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(User.objects.filter(username='testuser').exists())
 
-        self.assertTrue(User.objects.filter(username='newuser').exists())
-        self.assertRedirects(response, reverse('shopping-list'))
+    def test_user_login(self):
+        User.objects.create_user(username='testuser', password='StrongPass123')
+        response = self.client.post('/login/', {
+            'username': 'testuser',
+            'password': 'StrongPass123'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue('_auth_user_id' in self.client.session)
