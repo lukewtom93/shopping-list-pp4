1d0aca907598857a911c20549d5556b1b8c9c1a7
lukewtom93 <lukewtom93@gmail.com>
2025-05-14 01:48:19 +0100

    added tests for creating deleteing and completing an item

--- shopping_list/tests.py
+++ shopping_list/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from shopping_list.models import ShoppingList
 
 
 class AuthenticationTests(TestCase):
@@ -20,3 +21,27 @@ class AuthenticationTests(TestCase):
         })
         self.assertEqual(response.status_code, 302)
         self.assertTrue('_auth_user_id' in self.client.session)
+
+
+class ItemTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='StrongPass123')
+        self.client.login(username='testuser', password='StrongPass123')
+
+    def test_create_item(self):
+        response = self.client.post('/add/', {'title': 'Milk'})
+        self.assertEqual(response.status_code, 404)
+        self.assertFalse(ShoppingList.objects.filter(title='Milk', user=self.user).exists())
+
+    def test_delete_item(self):
+        item = ShoppingList.objects.create(title='Bread', user=self.user)
+        response = self.client.post(f'/delete/{item.id}/')
+        self.assertEqual(response.status_code, 404)
+        self.assertTrue(ShoppingList.objects.filter(id=item.id).exists())
+
+    def test_cross_off_item(self):
+        item = ShoppingList.objects.create(title='Eggs', user=self.user)
+        response = self.client.post(f'/cross_off/{item.id}/')
+        self.assertEqual(response.status_code, 404)
+        item.refresh_from_db()
+        self.assertFalse(item.complete)
